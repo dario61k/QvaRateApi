82e6dc0a16c57637b7afdc1e4ea9fbcf7637cbb7
dario61k <darioroque20033@gmail.com>
2025-07-06 17:20:33 -0400

    add health endpoint

--- internal/router.go
+++ internal/router.go
@@ -13,14 +13,11 @@ func SetupRoutes() *gin.Engine {
 	r := gin.Default()
 	r.Use(middleware.SetCors())
 
-	api := r.Group("/api")
-
-	api.GET("/ping", func(c *gin.Context) { // health check
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.HEAD("/health", func(c *gin.Context) { // health check
+		c.Status(200)
 	})
 
+	api := r.Group("/api")
 
 	api.GET("/get-currency/:startdate/:enddate", middleware.RateLimit(60, time.Minute*1, time.Minute*5), h.GetCurrency)
 	api.GET("/get-excel/:startdate/:enddate", middleware.RateLimit(6, time.Minute*1, time.Minute*5), h.GetExcel)
